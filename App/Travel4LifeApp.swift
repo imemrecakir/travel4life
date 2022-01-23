54ebc803fb9fbf379727a74f379c8e656a1e7dcc
Emre Çakır <imemrecakir@gmail.com>
2022-01-24 01:12:48 +0200

    Firebase Configuration

--- App/Travel4LifeApp.swift
+++ App/Travel4LifeApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Travel4LifeApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Travel4Life
+//
+//  Created by Emre Çakır on 24.01.2022.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
